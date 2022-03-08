45e68917f1169fd4cd53cdbb9760c7b77691d55f
arnxxau <arnxxau@gmail.com>
2022-03-09 00:31:47 +0100

    s2 finished

--- jutge.org/X22862.cpp
+++ jutge.org/X22862.cpp
@@ -9,14 +9,45 @@ double redondear(double r) {
 
 void round_cjt_mark(Cjt_estudiants& cjt) {
     int cjt_size = cjt.mida();
-    for (int i = 0; i < cjt_size; ++i) {
+    for (int i = 1; i <= cjt_size; ++i) {
         Estudiant est = cjt.consultar_iessim(i);
         if (est.te_nota()) {
-            int mark = redondear(est.consultar_nota());
+            double mark = redondear(est.consultar_nota());
             est.modificar_nota(mark);
             cjt.modificar_iessim(i, est);
         }
     }
+}
+
+void check_student_mark(const Cjt_estudiants& cjt) {
+     int dni;
+     cin >> dni;
+     if (cjt.existeix_estudiant(dni)) {
+         Estudiant est = cjt.consultar_estudiant(dni);
+         if (est.te_nota()) cout << "el estudiante " << dni << " tiene nota " 
+                                << est.consultar_nota() << endl << endl;
+         else cout << "el estudiante " << dni << " no tiene nota" << endl << endl;
+     } else 
+        cout << "el estudiante " << dni 
+                << " no esta" << endl << endl;
+}
+
+void modify_student_mark(Cjt_estudiants& cjt) {
+    Estudiant est;
+    est.llegir();
+    if (cjt.existeix_estudiant(est.consultar_DNI()))
+        cjt.modificar_estudiant(est);
+    else cout << "el estudiante " << est.consultar_DNI() 
+            << " no esta" << endl << endl; 
+}
+
+void add_student(Cjt_estudiants& cjt) {
+    Estudiant est;
+    est.llegir();
+    if (cjt.existeix_estudiant(est.consultar_DNI())) cout << "el estudiante " 
+                        << est.consultar_DNI() << " ya estaba" << endl << endl;
+    else if (cjt.mida_maxima() > cjt.mida()) cjt.afegir_estudiant(est);
+    else cout << "el conjunto esta lleno" << endl << endl;
 }
 
 int main() {
@@ -27,37 +58,14 @@ int main() {
     cin >> n;
     while (n != -6) {
         Estudiant est;
-        if (n == -1) { // -1 new student
-            est.llegir();
-            try {
-                cjt.afegir_estudiant(est);
-            }
-            catch(const PRO2Excepcio& e) {
-                cout << "el conjunto esta lleno" << endl;
-            }
-        } else if (n == -2) { // -2 check student mark
-            int dni;
-            cin >> dni;
-            est = cjt.consultar_estudiant(dni);
-            try {
-                 cout << "el estudiante " << dni << " tiene nota " << est.consultar_nota() << endl;
-            }
-            catch(const PRO2Excepcio& e) {
-                cout << "el estudiante " << dni << " no tiene nota" << endl;
-            }
-        } else if (n == -3) { // -3 modify student mark
-            est.llegir();
-            cjt.modificar_estudiant(est);
-        } else if (n == -4)
-        
-        
-        
+        if (n == -1) add_student(cjt); // -1 new student
+        else if (n == -2) check_student_mark(cjt);  // -2 check student mark
+        else if (n == -3) modify_student_mark(cjt); // -3 modify student mark
+        else if (n == -4) round_cjt_mark(cjt); // -4 round mark
+        else {
+            cjt.escriure();
+            cout << endl;
+        }// -5 write cjt
         cin >> n;
     }
-
-    
-    
-    
-    // -4 round mark
-    // -5 write cjt
 }
--- jutge.org/X94199.cpp
+++ jutge.org/X94199.cpp
@@ -1,45 +1,53 @@
 #include "Estudiant.hh"
 #include <vector>
 
- void simplificar_vector(vector<Estudiant>& v, int& pos) {
-     int v_size = v.size();
-     vector<Estudiant> V;
-     Estudiant est0 = v[0];
-     double max = 0;
-     for (int i = 0; i < v_size; ++i) {
-         Estudiant est1 = v[i];
-         if (est1.te_nota() and est1.consultar_nota() > max) {
-             max = est1.consultar_nota();
-             est0.modificar_nota(max);
-         }
-         if (est0.consultar_DNI() != est1.consultar_DNI()) {
-             V.push_back(est0);
-             est0 = est1;
-             max = est1.consultar_nota();
-         }
-     }
- }
+
  /* Pre: v=V, v.size() > 0 i les notes d'un mateix estudiant son consecutives */
  /* Post: pos+1 es el nombre d'estudiants diferents de V; cada estudiant de V
     nomes hi apareix un cop a v[0..pos], amb la nota mes alta i en el mateix
     ordre que a V */
+ void simplificar_vector(vector<Estudiant>& v, int& pos) {
+     int v_size = v.size();
+     vector<Estudiant> V;
+     pos = 0;
+     Estudiant est0 = v[0];
+     double max = 0;
+     Estudiant est1;
+     
+     for (int i = 0; i < v_size; ++i) {
+         est1 = v[i];
+         if (est0.consultar_DNI() != est1.consultar_DNI()) {
+             v[pos] = est0;
+             ++pos;
+             est0 = est1;
+             if (est1.te_nota()) max = est1.consultar_nota();
+             else max = 0;
+         }
+         else if (est1.te_nota() and est1.consultar_nota() > max) {
+             max = est1.consultar_nota();
+             est0 = est1;
+         }
+     }
+     v[pos] = est0;
+ }
 
-bool is_contained(int i, const vector<int>& idxs) { 
-    int v_size = idxs.size();
-    for (int j = 0; j < v_size; ++j)
-        if (idxs[j] == i) return true;
-    return false;
-}
 
-Estudiant get_average(int m, const vector<int>& idxs) {
-    double average = 0;
-    Estudiant est;
-    est.llegir();
-    double x = est.consultar_nota();
-    for (int i = 1; i < m + 1; ++i) {
-        if (is_contained(i, idxs)) average += x;
-        if (i < m) cin >> x;
+
+/*
+int main() {
+    int n;
+    cin >> n;
+    vector<Estudiant> estudiants(n);
+    for (int i  = 0; i < n; ++i) {
+        Estudiant est;
+        est.llegir();
+        estudiants[i] = est;
     }
-    est.modificar_nota(average / idxs.size());
-    return est;
+
+    int pos;
+    simplificar_vector(estudiants, pos);
+    cout << "pos: " << pos << endl;
+    for (int i = 0; i < estudiants.size(); ++i)
+        estudiants[i].escriure();
 }
+*/
